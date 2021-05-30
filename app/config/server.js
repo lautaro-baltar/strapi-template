39c0fa2e3b1bca5b7e757b350fa76245f7f6af4e
lautaro <lautaro.baltar@azumo.co>
2021-05-29 22:50:50 -0300

    initial commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f88e66ce10783432f74a8ac9c392bc0'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3f3d26a6-a0d9-4bd4-b3ea-1a53542cfa4a'
+};
